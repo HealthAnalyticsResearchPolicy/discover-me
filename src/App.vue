eeb6112163ad98d37a7ab4a9877c90bce75e97e0
James Lane <lanejames35@gmail.com>
2020-05-13 15:37:02 -0400

    mock up the nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <NavigationBar></NavigationBar>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,22 +8,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavigationBar
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-bar">
+    <b-navbar variant="info">
+      <b-navbar-brand
+        href="#"
+      >
+        DiscoverMe
+      </b-navbar-brand>
+      <b-navbar-nav
+        class="ml-auto"
+      >
+        <b-button>Fork us on GitHub</b-button>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NavigationBar'
+}
+</script>
