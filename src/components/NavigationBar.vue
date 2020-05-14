d983c0b7fb336539bde982f15ba3d470a875e185
James Lane <lanejames35@gmail.com>
2020-05-14 10:46:35 -0400

    add the icon to the navigation bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="nav-bar">
-    <b-navbar variant="info">
+    <b-navbar variant="primary">
       <b-navbar-brand
         href="#"
       >
-        DiscoverMe
+        <img src="../assets/bannerAvenir.png" alt="Discover Me" height="66px"/>
       </b-navbar-brand>
       <b-navbar-nav
         class="ml-auto"
       >
-        <b-button>Fork us on GitHub</b-button>
+        <b-button
+          variant="secondary"
+          size="lg"
+        >
+          Fork us on GitHub
+          <b-icon
+            icon="box-arrow-up-right"
+          ></b-icon>
+        </b-button>
       </b-navbar-nav>
     </b-navbar>
   </div>
