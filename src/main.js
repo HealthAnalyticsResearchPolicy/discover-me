e4bb8445fc0960d79f36c48ea464a2668836b5ff
James Lane <lanejames35@gmail.com>
2020-05-17 11:16:58 -0400

    installed vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import router from './router/index'
 import App from './App.vue'
 import './plugins/bootstrap-vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
